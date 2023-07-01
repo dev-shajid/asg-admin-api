c7e499e5c80e5572f873197c4c6b7ba676ae7995
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2023-07-01 23:44:51 +0600

    production static file bug fix

---
+++ src/client/build/assets/Home-c73cf5c2.js
@@ -0,0 +1,1 @@
+import{U as t,j as e}from"./index-86bc1542.js";import{T as r}from"./Basic-c25be6a5.js";const n=()=>{const{user:s}=t();return e.jsx(e.Fragment,{children:e.jsxs(r,{className:"text-center space-x-2 text-3xl",children:[e.jsx("span",{children:"Hello 👋,"})," ",e.jsx("span",{className:"capitalize",children:s.role})]})})};export{n as default};
--- src/index.js
+++ src/index.js
@@ -3,31 +3,30 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const cors = require('cors')
+const path = require('path')
 
 // Middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors({
-    origin: ['/', 'http://localhost:5173'] // TODO: Change to production URL when deployed
+    origin: '*' // TODO: Change to production URL when deployed
 
 }))
 
 // Database
 require('./db/dbConnect')()
 
-app.get('/', (req, res) => res.send("Hello World!"))
-
-// Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/user', require('./routes/userRoutes'));
-app.use('/api/course', require('./routes/courseRoutes'));
-
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.resolve(__dirname, "./client/build")));
     app.get("*", function (request, response) {
         response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/user', require('./routes/userRoutes'));
+app.use('/api/course', require('./routes/courseRoutes'));
 
 // Start the server
 const PORT = process.env.PORT || 8000;
