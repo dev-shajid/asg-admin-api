ff8c212006d49524bb43b845c627fcad94f37d6d
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2023-07-02 00:07:44 +0600

    production static file bug fix

--- src/client/build/assets/Home-315557c2.js
+++
@@ -1,1 +0,0 @@
-import{U as t,j as e}from"./index-3208b7ae.js";import{T as r}from"./Basic-d3189457.js";const n=()=>{const{user:s}=t();return e.jsx(e.Fragment,{children:e.jsxs(r,{className:"text-center space-x-2 text-3xl",children:[e.jsx("span",{children:"Hello 👋,"})," ",e.jsx("span",{className:"capitalize",children:s.role})]})})};export{n as default};
